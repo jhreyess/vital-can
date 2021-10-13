b9764ff822e64d38e040addf2676decb9a1aa284
Josué Reyes <josue.hreyess@gmail.com>
2021-10-13 15:28:35 -0500

    Add Heroku database

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('[server] MongoDB successfully connected'))
